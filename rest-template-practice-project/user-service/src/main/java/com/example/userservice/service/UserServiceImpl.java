89cf8d81b536499436ab5e82eb9dbbec34fb3b22
Ian Perfitt <iancp@protonnmail.com>
2024-05-21 12:28:17 -0500

    implemented User microservice using RestTemplate to call Department Microservice. Also renamed main folder to rest-template-practice-project

---
+++ rest-template-practice-project/user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -0,0 +1,54 @@
+package com.example.userservice.service;
+
+import com.example.userservice.dto.DepartmentDto;
+import com.example.userservice.dto.ResponseDto;
+import com.example.userservice.dto.UserDto;
+import com.example.userservice.entity.User;
+import com.example.userservice.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@AllArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+    private RestTemplate restTemplate;
+
+    @Override
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public ResponseDto getUser(Long userId) {
+
+        ResponseDto responseDto = new ResponseDto();
+        User user = userRepository.findById(userId).get();
+        UserDto userDto = mapToUser(user);
+
+        ResponseEntity<DepartmentDto> responseEntity = restTemplate
+                .getForEntity("http://localhost:8080/api/departments/" + user.getDepartmentId(),
+                        DepartmentDto.class);
+
+        DepartmentDto departmentDto = responseEntity.getBody();
+
+        System.out.println(responseEntity.getStatusCode());
+
+        responseDto.setUser(userDto);
+        responseDto.setDepartment(departmentDto);
+
+        return responseDto;
+    }
+
+    private UserDto mapToUser(User user){
+        UserDto userDto = new UserDto();
+        userDto.setId(user.getId());
+        userDto.setFirstName(user.getFirstName());
+        userDto.setLastName(user.getLastName());
+        userDto.setEmail(user.getEmail());
+        return userDto;
+    }
+}
